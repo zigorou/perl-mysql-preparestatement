e247d075e2783fdbe7d86317b8f236aa7eb30252
zigorou <zigorou@cpan.org>
2012-08-08 21:12:40 +0900

    initial commit

---
+++ lib/MySQL/PreparedStatement.pm
@@ -0,0 +1,32 @@
+package MySQL::PreparedStatement;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+MySQL::PreparedStatement -
+
+=head1 SYNOPSIS
+
+  use MySQL::PreparedStatement;
+
+=head1 DESCRIPTION
+
+MySQL::PreparedStatement is
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
